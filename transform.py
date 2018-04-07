52106c70e85fe6d7f73b48fb5f3f67f221c0a0c6
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2018-04-07 15:50:35 +0100

    Implemented Magnitude Threshhold and Directional Threshold function

--- transform.py
+++ transform.py
@@ -7,7 +7,7 @@ import pickle
 
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Calculate directional gradient
     # Apply threshold
     direction = (1, 0)
@@ -26,24 +26,54 @@ def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
     # Calculate gradient magnitude
     # Apply threshold
 
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    mag_binary = np.zeros_like(gradmag)
+    mag_binary[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 255
     return mag_binary
 
 def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
     # Calculate gradient direction
     # Apply threshold
+
+    # Grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction,
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    dir_binary =  np.zeros_like(absgraddir)
+    dir_binary[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 255
     return dir_binary
 
 # Choose a Sobel kernel size
 ksize = 3 # Choose a larger odd number to smooth gradient measurements
 
-image = mpimg.imread('test_images/signs_vehicles_xygrad.png')
+image = cv2.imread('test_images/signs_vehicles_xygrad.png')
 
 # Apply each of the thresholding functions
 gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
 grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
-#mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
-#dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
 
-cv2.imshow('Gradient Binary',gradx)
+cv2.imshow('Gradient X Binary',gradx)
+cv2.waitKey()
+cv2.imshow('Gradient Y Binary',grady)
+cv2.waitKey()
+cv2.imshow('Mag Thresh Binary',mag_binary)
+cv2.waitKey()
+cv2.imshow('Dir Thresh Binary',dir_binary)
 cv2.waitKey()
 cv2.destroyAllWindows()
