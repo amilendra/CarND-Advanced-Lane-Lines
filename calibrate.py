72f928528e3fd5c842d96e7f732d9eb68b4de824
amilendra <amilendra.kodithuwakku@gmail.com>
2018-04-09 23:22:04 +0100

    Cleanup for submission

--- calibrate.py
+++ calibrate.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
+import os
 import glob
-import matplotlib.pyplot as plt
 import pickle
 
+CHESSBOARD_X_SQUARES = 9
+CHESSBOARD_Y_SQUARES = 6
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+objp = np.zeros((CHESSBOARD_Y_SQUARES*CHESSBOARD_X_SQUARES,3), np.float32)
+objp[:,:2] = np.mgrid[0:CHESSBOARD_X_SQUARES, 0:CHESSBOARD_Y_SQUARES].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d points in real world space
@@ -21,7 +23,7 @@ for idx, fname in enumerate(images):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_X_SQUARES,CHESSBOARD_Y_SQUARES), None)
 
     # If found, add object points, image points
     if ret == True:
@@ -29,10 +31,10 @@ for idx, fname in enumerate(images):
         imgpoints.append(corners)
 
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (9,6), corners, ret)
-        #write_name = 'corners_found'+str(idx)+'.jpg'
-        #cv2.imwrite(write_name, img)
-        cv2.imshow('img', img)
+        cv2.drawChessboardCorners(img, (CHESSBOARD_X_SQUARES,CHESSBOARD_Y_SQUARES), corners, ret)
+        write_name = 'output_images/corners_' + os.path.basename(fname)
+        cv2.imwrite(write_name, img)
+        cv2.imshow(write_name, img)
         cv2.waitKey()
 
 
@@ -48,7 +50,6 @@ cv2.imwrite('output_images/test_dist.jpg',img)
 # Do camera calibration given object points and image points
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
 
-
 dst = cv2.undistort(img, mtx, dist, None, mtx)
 cv2.imwrite('output_images/test_undist.jpg',dst)
 
@@ -57,7 +58,7 @@ dist_pickle = {}
 dist_pickle["mtx"] = mtx
 dist_pickle["dist"] = dist
 pickle.dump( dist_pickle, open( "output_images/wide_dist_pickle.p", "wb" ) )
-#dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+
 # Visualize undistortion
 cv2.imshow('Undistorted Image', dst)
 cv2.waitKey()
